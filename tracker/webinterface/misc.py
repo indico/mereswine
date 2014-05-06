52151f6b5c08c67cfeb0fdf7661d66bf5461e100
Tommaso Papini <tommaso.papini@cern.ch>
2014-05-06 11:52:49 +0200

    Remove icon for server list

--- tracker/webinterface/misc.py
+++ tracker/webinterface/misc.py
@@ -1,5 +1,6 @@
-from flask import render_template, g
+from flask import render_template, g, redirect, url_for, request
 
+from ..core import db
 from ..menu import menu, breadcrumb, make_breadcrumb
 from ..models import Instance
 from . import bp
@@ -12,12 +13,20 @@ def index():
     return render_template('index.html')
 
 
-@bp.route('/server-list')
+@bp.route('/servers/server-list')
 @menu('server_list')
 @breadcrumb('List of servers', '.server_list')
 def server_list():
     g.server_list = Instance.query.filter_by(enabled=True).all()
     return render_template('server_list.html')
+
+
+@bp.route('/servers/remove-server/<uuid>')
+def remove_server(uuid):
+    instance = Instance.query.filter_by(uuid=uuid).one()
+    db.session.delete(instance)
+    db.session.commit()
+    return redirect(url_for('.server_list'))
 
 
 @bp.route('/statistics')
